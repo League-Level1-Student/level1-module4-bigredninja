976ff7bb40873b6c5a4533faa05ff519e0785430
C2SDK <C2SDK@C47427>
2020-05-27 19:32:55 -0700

    asdfghjk

--- src/_05_typing_tutor/TypingTouter.java
+++ src/_05_typing_tutor/TypingTouter.java
@@ -1,5 +1,26 @@
 package _05_typing_tutor;
 
-public class TypingTouter {
+import java.util.Random;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class TypingTouter {
+	char currentLetter;
+	
+	char generateRandomLetter() {
+	    Random r = new Random();
+	    return (char) (r.nextInt(26) + 'a');
+	}
+public void run () {
+	currentLetter = generateRandomLetter();
+JFrame frame = new JFrame();
+JLabel label = new JLabel();
+	label.setText("currentLetter");
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.add(label);
+	frame.pack();
 }
+}
+
